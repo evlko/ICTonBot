69315932130fda2e175f13559149b6ce8b5a07cc
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-11-21 02:38:33 +0300

    ✨ Added welcome message text and buttons 🔨 Some structure improvements and changes

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ selected_subjects = []
 
 @bot.message_handler(commands=['start', 'help'])
 def start_messaging(message):
+    subjects = ['Начать общение', 'О создателях']
 
-    ask_for_subjects(message.chat.id)
-    # subjects = ['Дискретка', 'Линал', 'Матан', 'Аналгеом', 'Физика', 'Готово']
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    for each in subjects:
+        markup.add(telebot.types.InlineKeyboardButton(text=each, callback_data=each))
 
+    with open('text_messages/welcome_message.txt', 'rt', encoding='utf-8') as f:
+        text = f.read()
 
-#     # markup = telebot.types.InlineKeyboardMarkup(row_width=1)
-#     # for each in subjects:
-#     #     markup.add(telebot.types.InlineKeyboardButton(text=each, callback_data=each))
-#     #
-#     # bot.reply_to(message, "Привет, выбери предметы, по которым тебе нужна помощь:", reply_markup=markup)
+    bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -26,6 +26,12 @@ def handle_query(call):
     if call.data == 'Готово':
         bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.send_message(call.message.chat.id, 'Ну и хорошо')
+    elif call.data == 'Начать общение':
+        ask_for_subjects(call.message.chat.id)
+    elif call.data == 'О создателях':
+        with open('text_messages/about.txt', 'rt', encoding='utf-8') as f:
+            text = f.read()
+        bot.send_message(call.message.chat.id, text)
     else:
         if call.data in selected_subjects:
             selected_subjects.remove(call.data)
@@ -43,7 +49,7 @@ def ask_for_subjects(chat_id: int):
             markup.add(telebot.types.InlineKeyboardButton(text=each + " ✅", callback_data=each))
         else:
             markup.add(telebot.types.InlineKeyboardButton(text=each, callback_data=each))
-    bot.send_message(chat_id, "Привет, выбери предметы, по которым тебе нужна помощь:", reply_markup=markup)
+    bot.send_message(chat_id, "Пожалуйста, выбери предметы, по которым тебе нужна помощь:", reply_markup=markup)
 
 
 if __name__ == '__main__':
