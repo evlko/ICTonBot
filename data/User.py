5098908d9783d19d1053f019c16197a064965b65
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-11-21 12:16:19 +0300

    📚 Some documentation in User class

--- data/User.py
+++ data/User.py
@@ -7,21 +7,26 @@ class User:
     """Data class that stores user data."""
     name: str
     faculty: str
-    need: typing.List[str]
-    give: typing.List[str]
+    need_subjects: typing.List[str]
+    give_subjects: typing.List[str]
     user_hash: int
 
     def __init__(self, name: str, faculty: str, need: typing.List[str], give: typing.List[str]):
+        """A class constructor that automatically generates user hash."""
+
         self.name = name
         self.faculty = faculty
-        self.need = need
-        self.give = give
+        self.need_subjects = need
+        self.give_subjects = give
         user_hash = components.core.generate_hash()
 
-    def to_dict(self):
+    def to_dict(self) -> typing.Dict:
+        """A method that is used for user data serialization to dictionary."""
+
         return {
             "name": self.name,
             "faculty": self.faculty,
-            "need": self.need,
-            "give": self.give
+            "need": self.need_subjects,
+            "give": self.give_subjects,
+            "hash": self.user_hash
         }
