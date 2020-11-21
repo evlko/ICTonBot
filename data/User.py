8b179ac9e69f4f70ae471cd18c432562d4ccca83
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-11-21 12:03:00 +0300

    🔨 User.py moved to "data" directory ✨ Created file Chat.py with Chat data class in "data" directory

--- data/User.py
+++ data/User.py
@@ -1,5 +1,7 @@
 import typing
 
+import components.core
+
 
 class User:
     """Data class that stores user data."""
@@ -7,12 +9,14 @@ class User:
     faculty: str
     need: typing.List[str]
     give: typing.List[str]
+    user_hash: int
 
     def __init__(self, name: str, faculty: str, need: typing.List[str], give: typing.List[str]):
         self.name = name
         self.faculty = faculty
         self.need = need
         self.give = give
+        user_hash = components.core.generate_hash()
 
     def to_dict(self):
         return {
