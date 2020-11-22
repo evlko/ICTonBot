b741e1809991dd85113bd74d2d363ed68f89e902
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2020-11-22 12:21:14 +0300

    🔨 Changed some tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,7 +8,7 @@ class GenerateUserTestCase(unittest.TestCase):
 
     @staticmethod
     def test_new_user():
-        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"])
+        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"], "3")
 
     @staticmethod
     def test_fake_user():
@@ -16,20 +16,20 @@ class GenerateUserTestCase(unittest.TestCase):
 
     @staticmethod
     def test_user_to_dict():
-        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"])
+        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"], UserState.START.value[0])
         print(user.to_dict())
 
     @staticmethod
     def test_user_from_dict():
-        data = {"name": "Name", "faculty": "Faculty", "need": ["a", "b"], "give": ["c", "d"]}
+        data = {"name": "Name", "faculty": "Faculty", "need": ["a", "b"], "give": ["c", "d"], "state": "3"}
         print(UserFactory.from_dict(data))
 
     @staticmethod
     def test_serialization():
-        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"])
+        user = UserFactory.new_user("Name", "Faculty", ["a", "b"], ["c", "d"], UserState.ABOUT.value[0])
         print(serialized(user))
 
     @staticmethod
     def test_deserialization():
-        s = '{"name": "Name", "faculty": "Faculty", "need": ["a", "b"], "give": ["c", "d"]}'
+        s = '{"name": "Name", "faculty": "Faculty", "need": ["a", "b"], "give": ["c", "d"], "state": "2"}'
         print(deserialized(s))
